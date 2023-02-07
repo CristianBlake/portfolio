51a6d03a8a4f91a6bf157b4f972e39d62cffdd95
Cristian J. López <cristianblake.dev@gmail.com>
2023-02-06 21:24:18 -0600

    fix: Mobile clouds position

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,21 @@
 <template>
-  <main class="w-screen h-screen bg-sky-900">
+  <main class="h-screen main-bg md:main-bg-lg">
     <section class="h-full grid grid-cols-1 justify-items-center content-center">
         <HomeTitle />
     </section>
     <Clouds />
   </main>
+  <section>
+    <h2>Trabajo</h2>
+  </section>
 </template>
+
+<style scoped>
+.main-bg {
+  background: linear-gradient(180deg, rgba(12,74,110,1) 54%, rgba(255,255,255,1) 100%);
+}
+
+.main-bg-lg {
+  background: #0c4a6e;
+}
+</style>
